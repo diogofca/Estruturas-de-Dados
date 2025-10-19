26695a13e842876b8ed5908c158b15b203110134
Diogo Almeida <diogofca@outlook.com>
2025-10-19 19:02:41 +0100

    finalizar semana 5

--- Week 5/TestBigNumber.java
+++ Week 5/TestBigNumber.java
@@ -1,26 +1,37 @@
 public class TestBigNumber {
     public static void main(String[] args) {
-        BigNumber n1 = new BigNumber("1234567890");
-        System.out.println(n1); // Escreve "1234567890"
-
-        BigNumber n2 = new BigNumber("42");
-        BigNumber n3 = new BigNumber("1234567890");
-        System.out.println(n1.equals(n2)); // Escreve "false"
-        System.out.println(n1.equals(n3)); // Escreve "true"
-
-        BigNumber n4 = new BigNumber("46711237126582920746212");
-        BigNumber n5 = new BigNumber("8765432110");
-        BigNumber n6 = n1.add(n3);
-        System.out.println(n6); // Escreve "2469135780"
-        BigNumber n7 = n1.add(n4);
-        System.out.println(n7); // Escreve "46711237126584155314102"
-        BigNumber n8 = n1.add(n5);
-        System.out.println(n8); // Escreve "10000000000"
-
-        // BigNumber n9 = n1.multiply(n3);
-        // System.out.println(n9); // Escreve "1524157875019052100"
-        // BigNumber n10 = n1.multiply(n4);
-        // System.out.println(n10); // Escreve "57668193458655139375688174332680"
+        BigNumber n1 = new BigNumber("1");
+        System.out.println(n1);
+        BigNumber n2 = new BigNumber("2");
+        BigNumber n3 = new BigNumber("2361736176326363");
+        BigNumber n4 = new BigNumber("7362767647676737");
+        BigNumber n5 = new BigNumber("12378123783987389174692846829");
+        BigNumber n6 = new BigNumber("87621876216012610825307153171");
+        BigNumber n7 = new BigNumber("28347923749823749872395872985798598579585");
+        BigNumber n8 = n1.add(n2);
+        BigNumber n9 = n3.add(n2);
+        BigNumber n10 = n3.add(n4);
+        BigNumber n11 = n4.add(n5);
+        BigNumber n12 = n6.add(n5);
+        BigNumber n13 = n5.add(n6);
+        BigNumber n14 = n6.add(n7);
+        BigNumber n15 = n14.add(n14);
+        BigNumber n16 = n15.add(n14);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
+        System.out.println(n5);
+        System.out.println(n6);
+        System.out.println(n7);
+        System.out.println(n8);
+        System.out.println(n9);
+        System.out.println(n10);
+        System.out.println(n11);
+        System.out.println(n12);
+        System.out.println(n13);
+        System.out.println(n14);
+        System.out.println(n15);
+        System.out.println(n16);
     }
 }
 
@@ -29,8 +40,8 @@ class BigNumber {
 
     BigNumber(String number) {
         this.digits = new int[number.length()];
-        for (int i = 0; i < digits.length; i++) {
-            this.digits[i] = (number.charAt(number.length() - i) - '0');
+        for (int i = 0; i < number.length(); i++) {
+            this.digits[i] = (number.charAt(number.length() - 1 - i) - '0');
         }
     }
 
@@ -55,7 +66,7 @@ class BigNumber {
     @Override
     public String toString() {
         StringBuilder toReturn = new StringBuilder();
-        for (int i = 0; i < this.size(); i++) {
+        for (int i = size() - 1; i >= 0; i--) {
             toReturn.append(String.format("%d", this.digits[i]));
         }
         return toReturn.toString();
@@ -75,9 +86,12 @@ class BigNumber {
             for (int i = 0; i < n.size(); i++) {
                 toReturn.append(String.format("%d", (otherNumber[i] + n.digits[i] + goesOne) % 10));
                 goesOne = (otherNumber[i] + n.digits[i] + goesOne) / 10;
+                // System.out.println(String.format("%d", (otherNumber[i] + n.digits[i] +
+                // goesOne) % 10));
+                // System.out.println(goesOne);
             }
-            if (goesOne == 1) {
-                toReturn.append("1");
+            if (goesOne > 0) {
+                toReturn.append(String.format("%d", goesOne));
             }
 
         } else {
@@ -86,16 +100,56 @@ class BigNumber {
                 otherNumber[i] = n.digits[i];
             }
             int goesOne = 0;
-            for (int i = 0; i < n.size(); i++) {
+            for (int i = 0; i < this.size(); i++) {
                 toReturn.append(String.format("%d", (otherNumber[i] + this.digits[i] + goesOne) % 10));
                 goesOne = (otherNumber[i] + this.digits[i] + goesOne) / 10;
             }
-            if (goesOne == 1) {
-                toReturn.append("1");
+            if (goesOne > 0) {
+                toReturn.append(String.format("%d", goesOne));
             }
 
         }
 
-        return new BigNumber(toReturn.toString());
+        return new BigNumber(toReturn.reverse().toString());
+    }
+
+    public BigNumber multiply(BigNumber n) {
+        BigNumber[] toReturnList = new BigNumber[n.size()];
+        for (int i = 0; i < n.size(); i++) {
+            StringBuilder toAdd = new StringBuilder();
+            int constant = n.digits[i];
+            // System.out.println(constant);
+            int goes = 0;
+            for (int k = 0; k < i; k++) {
+                toAdd.append("0");
+            }
+            for (int j = 0; j < this.size(); j++) {
+                // System.out.println(constant * this.digits[j] + goes);
+                int lala = (constant * this.digits[j] + goes) % 10;
+                goes = (constant * this.digits[j] + goes) / 10;
+                toAdd.append(String.format("%d", lala));
+                // System.out.println(goes);
+                // System.out.println(lala);
+            }
+            if (goes > 0) {
+                toAdd.append(String.format("%d", goes));
+            }
+            toReturnList[i] = new BigNumber(toAdd.reverse().toString());
+            // System.out.println(toReturnList[i]);
+        }
+        if (toReturnList.length == 1) {
+            return toReturnList[0];
+        }
+        BigNumber toReturn = toReturnList[0].add(toReturnList[1]);
+        // System.out.println(toReturn);
+        for (int i = 2; i < toReturnList.length; i++) {
+            // System.out.println(toReturnList[i]);
+            toReturn = toReturn.add(toReturnList[i]);
+            // System.out.println(toReturn);
+
+        }
+
+        return toReturn;
+
     }
 }
--- Week 5/toReturn.java
+++ Week 5/toReturn.java
@@ -1,73 +1,117 @@
-class BooleanArrayIntSet implements IntSet {
-    // Deverá colocar aqui os atributos e métodos
-    private boolean[] elems;
-    private int size;
 
-    BooleanArrayIntSet(int n) {
-        this.elems = new boolean[n + 1];
-        this.size = 0;
-    }
+class BigNumber {
+    protected int[] digits;
 
-    public IntSet intersection(IntSet s) {
-        BooleanArrayIntSet toReturn = new BooleanArrayIntSet(this.elems.length);
-        for (int i = 0; i < this.elems.length; i++) {
-            if (this.contains(i) && s.contains(i)) {
-                toReturn.add(i);
-            }
+    BigNumber(String number) {
+        this.digits = new int[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            this.digits[i] = (number.charAt(number.length() - 1 - i) - '0');
         }
-        return toReturn;
     }
 
-    public boolean equals(IntSet s) {
-        for (int i = 0; i < this.elems.length; i++) {
-            if (this.elems[i]) {
-                if (!s.contains(i)) {
+    protected int size() {
+        return this.digits.length;
+    }
+
+    public boolean equals(BigNumber n) {
+        if (n.size() != this.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < this.size(); i++) {
+                if (this.digits[i] != n.digits[i]) {
                     return false;
                 }
             }
-        }
-        return this.size == s.size();
-    }
-
-    public void clear() {
-        for (int i = 0; i < this.elems.length; i++) {
-            this.elems[i] = false;
-        }
-        size = 0;
-    }
-
-    public int size() {
-        return this.size;
-    }
-
-    public boolean contains(int x) {
-        return this.elems[x];
-    }
-
-    public boolean add(int x) {
-        if (this.elems[x] == true) {
-            return false;
-        }
-        try {
-            this.elems[x] = true;
-            size += 1;
             return true;
-        } catch (IndexOutOfBoundsException e) {
-            return false;
         }
+
     }
 
-    public boolean remove(int x) {
-        if (this.elems[x] == false) {
-            return false;
+    @Override
+    public String toString() {
+        StringBuilder toReturn = new StringBuilder();
+        for (int i = size() - 1; i >= 0; i--) {
+            toReturn.append(String.format("%d", this.digits[i]));
         }
-        try {
-            this.elems[x] = false;
-            size -= 1;
-            return true;
-        } catch (IndexOutOfBoundsException e) {
-            return false;
-        }
+        return toReturn.toString();
     }
 
+    public BigNumber add(BigNumber n) {
+        StringBuilder toReturn = new StringBuilder();
+
+        if (n.size() > this.size()) {
+            int[] otherNumber = new int[n.size()];
+            for (int i = 0; i < this.size(); i++) {
+                otherNumber[i] = this.digits[i];
+            }
+
+            // já tenho as coisas iguais
+            int goesOne = 0;
+            for (int i = 0; i < n.size(); i++) {
+                toReturn.append(String.format("%d", (otherNumber[i] + n.digits[i] + goesOne) % 10));
+                goesOne = (otherNumber[i] + n.digits[i] + goesOne) / 10;
+                // System.out.println(String.format("%d", (otherNumber[i] + n.digits[i] +
+                // goesOne) % 10));
+                // System.out.println(goesOne);
+            }
+            if (goesOne > 0) {
+                toReturn.append(String.format("%d", goesOne));
+            }
+
+        } else {
+            int[] otherNumber = new int[this.size()];
+            for (int i = 0; i < n.size(); i++) {
+                otherNumber[i] = n.digits[i];
+            }
+            int goesOne = 0;
+            for (int i = 0; i < n.size(); i++) {
+                toReturn.append(String.format("%d", (otherNumber[i] + this.digits[i] + goesOne) % 10));
+                goesOne = (otherNumber[i] + this.digits[i] + goesOne) / 10;
+            }
+            if (goesOne > 0) {
+                toReturn.append(String.format("%d", goesOne));
+            }
+
+        }
+
+        return new BigNumber(toReturn.reverse().toString());
+    }
+
+    public BigNumber multiply(BigNumber n) {
+        BigNumber[] toReturnList = new BigNumber[n.size()];
+        for (int i = 0; i < n.size(); i++) {
+            StringBuilder toAdd = new StringBuilder();
+            int constant = n.digits[i];
+            // System.out.println(constant);
+            int goes = 0;
+            for (int k = 0; k < i; k++) {
+                toAdd.append("0");
+            }
+            for (int j = 0; j < this.size(); j++) {
+                // System.out.println(constant * this.digits[j] + goes);
+                int lala = (constant * this.digits[j] + goes) % 10;
+                goes = (constant * this.digits[j] + goes) / 10;
+                toAdd.append(String.format("%d", lala));
+                // System.out.println(goes);
+                // System.out.println(lala);
+            }
+            if (goes > 0) {
+                toAdd.append(String.format("%d", goes));
+            }
+            toReturnList[i] = new BigNumber(toAdd.reverse().toString());
+            // System.out.println(toReturnList[i]);
+        }
+
+        BigNumber toReturn = toReturnList[0].add(toReturnList[1]);
+        // System.out.println(toReturn);
+        for (int i = 2; i < toReturnList.length; i++) {
+            // System.out.println(toReturnList[i]);
+            toReturn = toReturn.add(toReturnList[i]);
+            // System.out.println(toReturn);
+
+        }
+
+        return toReturn;
+
+    }
 }
